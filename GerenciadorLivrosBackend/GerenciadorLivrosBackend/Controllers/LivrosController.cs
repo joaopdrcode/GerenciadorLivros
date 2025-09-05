7f35d7be2ccafcd4500b595040353fb34c4c7952
joaopdrcode <joaopedro.lpr10@gmail.com>
2025-09-04 23:42:26 -0300

    Controlador
    - Configuracao de repositorios e do service no controlador
    - Definicao de metodos, endpoints e logicas

---
+++ GerenciadorLivrosBackend/GerenciadorLivrosBackend/Controllers/LivrosController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GerenciadorLivrosBackend.DTOs;
+using GerenciadorLivrosBackend.Models;
+using GerenciadorLivrosBackend.Services;
+
+namespace GerenciadorLivrosBackend.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class LivrosController : Controller
+    {
+        private readonly ApiResponseService _apiResponseService;
+        public readonly IGetLivroRepository _getLivroRepository;
+        public readonly IGetLivrosRepository _getLivrosRepository;
+        public readonly IDeleteLivroRepository _deleteLivroRepository;
+        public readonly IInsertLivroRepository _insertLivroRepository;
+        public readonly IUpdateLivroRepository _updateLivroRepository;
+
+        public LivrosController(IGetLivroRepository getLivroRepository, IGetLivrosRepository getLivrosRepository, IDeleteLivroRepository deleteLivroRepository, IInsertLivroRepository insertLivroRepository, IUpdateLivroRepository updateLivroRepository, ApiResponseService responseService)
+        {
+            _getLivroRepository = getLivroRepository;
+            _getLivrosRepository = getLivrosRepository;
+            _deleteLivroRepository = deleteLivroRepository;
+            _insertLivroRepository = insertLivroRepository;
+            _updateLivroRepository = updateLivroRepository;
+            _apiResponseService = responseService;
+        }
+
+        [HttpGet]
+        [Route("livros")]
+        public async Task<IActionResult> GetLivros()
+        {
+            var livros = await _getLivrosRepository.GetLivros();
+            return Ok(_apiResponseService.ApiResponse(true, "Consulta Realizada!", livros));
+        }
+
+        [HttpGet]
+        [Route("livros/{id}")]
+        public async Task<IActionResult> GetLivro(int id)
+        {
+            var livro = await _getLivroRepository.GetLivro(id);
+            if (livro != null) return Ok(_apiResponseService.ApiResponse(true, "Consulta Realizada!", livro));
+            return NotFound(_apiResponseService.ApiResponse(false, "Erro Durante a Consulta"));
+        }
+
+        [HttpPost]
+        [Route("livros")]
+        public async Task<IActionResult> InsertLivro(LivroDTO livroDTO)
+        {
+            var livro = new Livro
+            {
+                Ano = livroDTO.Ano,
+                Autor = livroDTO.Autor,
+                Genero = livroDTO.Genero,
+                Titulo = livroDTO.Titulo
+            };
+            var addLivro = await _insertLivroRepository.InsertLivro(livro);
+            if(addLivro) return Ok(_apiResponseService.ApiResponse(true, "Livro Adicionado com Sucesso!"));
+            return NotFound(_apiResponseService.ApiResponse(false, "Erro ao Adicionar Livro"));
+        }
+
+        [HttpPut]
+        [Route("livros/{id}")]
+        public async Task<IActionResult> UpdateLivro(int id, LivroDTO livroDTO)
+        {
+            var livro = new Livro
+            {
+                Id = id,
+                Ano = livroDTO.Ano,
+                Autor = livroDTO.Autor,
+                Genero = livroDTO.Genero,
+                Titulo = livroDTO.Titulo
+            };
+            var addLivro = await _updateLivroRepository.UpdateLivro(livro);
+            if (addLivro) return Ok(_apiResponseService.ApiResponse(true, "Livro Atualizado com Sucesso!"));
+            return NotFound(_apiResponseService.ApiResponse(false, "Erro ao Atualizar Livro"));
+        }
+
+        [HttpDelete]
+        [Route("livros/{id}")]
+        public async Task<IActionResult> DeleteLivro(int id)
+        {
+            var deleteLivro = await _deleteLivroRepository.DeleteLivro(id);
+            if (deleteLivro) return Ok(_apiResponseService.ApiResponse(true, "Livro Removido com Sucesso!"));
+            return NotFound(_apiResponseService.ApiResponse(false, "Erro ao Remover Livro"));
+        }
+    }
+}
