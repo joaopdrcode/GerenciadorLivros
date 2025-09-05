4c1d529bc6cb4b907be85bd77de2843cddbccfcf
joaopdrcode <joaopedro.lpr10@gmail.com>
2025-09-04 23:12:04 -0300

    Service com Estrutura da Resposta da API
    Service com estrutura dinamica para retornar informacoes das requisicoes

---
+++ GerenciadorLivrosBackend/GerenciadorLivrosBackend/Services/ApiResponseService.cs
@@ -0,0 +1,22 @@
+﻿namespace GerenciadorLivrosBackend.Services
+{
+    public class ApiResponseService
+    {
+        public class Response
+        {
+            public bool Status { get; set; }
+            public string Mensagem { get; set; }
+            public object? Dados { get; set; }
+        }
+
+        public Response ApiResponse (bool status, string mensagem, object dados = null)
+        {
+            return new Response
+            {
+                Status = status,
+                Mensagem = mensagem,
+                Dados = dados
+            };
+        }
+    }
+}
